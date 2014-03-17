e97a03b28d942ebab9e9e4a9a32d5905aa97ad39
Silvan Spross <silvan.spross@gmail.com>
2014-03-17 14:14:35 +0100

    removed license

--- project.clj
+++ project.clj
@@ -2,8 +2,6 @@
   :description "official website of the balerunner game"
   :url "http://balerunner.ch"
   :min-lein-version "2.0.0"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.3"]
                  [postgresql "9.1-901.jdbc4"]
