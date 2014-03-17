c79466fd87485ce9d93013d55457d0aa74316f1e
Silvan Spross <silvan.spross@gmail.com>
2014-03-17 12:31:39 +0100

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to balerunner
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/balerunner/core_test.clj
@@ -0,0 +1,7 @@
+(ns balerunner.core-test
+  (:require [clojure.test :refer :all]
+            [balerunner.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
